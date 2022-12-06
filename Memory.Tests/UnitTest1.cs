525ed7690b4f5e493d23f4bbb720b3480b229970
Aurélien BOUDOUX <aurelien@boudoux.fr>
2022-12-06 13:51:54 +0100

    Add project files.

---
+++ Memory.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+using FluentAssertions;
+
+namespace Memory.Tests {
+    public class UnitTest1 
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
---
+++ MemoryEngine/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace MemoryEngine {
+    public class Class1 
+    {
+
+    }
+}
